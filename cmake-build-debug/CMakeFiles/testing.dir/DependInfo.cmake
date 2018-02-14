ef8656d0c47492cabfdd6f93a40660bf4e423ea2
Rendall Schijven <rendall.schijven@hotmail.com>
2018-02-14 17:04:16 +0100

    Upload all files to git

--- cmake-build-debug/CMakeFiles/testing.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/testing.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/c/Users/renda/Desktop/Git/algoritmiek/testing.cpp" "/cygdrive/c/Users/renda/Desktop/Git/algoritmiek/cmake-build-debug/CMakeFiles/testing.dir/testing.cpp.o"
+  "/cygdrive/c/Users/renda/Desktop/Git/Algoritmiek/testing.cpp" "/cygdrive/c/Users/renda/Desktop/Git/Algoritmiek/cmake-build-debug/CMakeFiles/testing.dir/testing.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
